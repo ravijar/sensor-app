3082e9ae1806ae5efac7fdd56e4a0673f67a5f5c
ravijar <ravijarandinu@gmail.com>
2025-03-06 15:00:55 +0530

    fixed issues

--- app/src/main/java/com/littlebits/sensorapp/sensor/GameRotationVector.java
+++ app/src/main/java/com/littlebits/sensorapp/sensor/GameRotationVector.java
@@ -3,7 +3,7 @@ package com.littlebits.sensorapp.sensor;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 
-public class GameRotationVector extends XYZABFloatSensor {
+public class GameRotationVector extends XYZFloatSensor {
 
     public GameRotationVector(SensorManager sensorManager) {
         super(sensorManager, Sensor.TYPE_GAME_ROTATION_VECTOR);
