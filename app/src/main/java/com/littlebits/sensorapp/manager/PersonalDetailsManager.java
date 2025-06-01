77d1b0a6e39278fe716987e1b2f7bc716c9a30d7
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2025-06-02 02:12:50 +0530

    personal details ui bug fixes

--- app/src/main/java/com/littlebits/sensorapp/manager/PersonalDetailsManager.java
+++ app/src/main/java/com/littlebits/sensorapp/manager/PersonalDetailsManager.java
@@ -66,7 +66,7 @@ public class PersonalDetailsManager {
         if (bmi < 18.5) {
             return "Underweight";
         } else if (bmi >= 18.5 && bmi <= 24.9) {
-            return "Normal weight";
+            return "Normal Weight";
         } else if (bmi >= 25 && bmi <= 29.9) {
             return "Overweight";
         } else {
